133784b2694e0ad3088baaeaf4be27db673ec8fc
Dobber Peas <dobberpe@student.21-school.ru>
2024-02-18 18:38:50 +0300

    dec to float start. dec to int not tested

--- src/s21_decimal_converter.c
+++ src/s21_decimal_converter.c
@@ -21,16 +21,16 @@ int s21_from_float_to_decimal(float src, s21_decimal *dst) {   // перевод
     if (f.bits && (e < -93 || e > 95)) {
         res = 1;
     } else if (f.bits) {
-        if ((e >= 23 && e <= 31) || (e >= 55 && e <= 63) || (e >= 87 && e <= 95)) {
+        if (e % 32 >= 23 && e % 32 <= 31) {
             unsigned int_bits = (f.bits << 9) >> 9;
-            int_bits = (int_bits << (e % (e < 55 ? 23 : e < 87 ? 55 : 87))) | ((unsigned)1 << (e % 32));
-            dst->bits[e < 55 ? 0 : e < 87 ? 1 : 2] = int_bits;
+            int_bits = (int_bits << (e % (32 * (e / 32) + 23))) | ((unsigned)1 << (e % 32));
+            dst->bits[e / 32] = int_bits;
         } else if (e > 31) { // нужно проверить пограничные случаи
             unsigned int_bits = (f.bits << 9) >> 9;
             int_bits = (int_bits >> (24 - e % (e > 63 ? 63 : 31))) | ((unsigned)1 << (e % (e > 63 ? 63 : 31) - 1));
-            dst->bits[e > 63 ? 2 : 1] = int_bits;
+            dst->bits[e / 32] = int_bits;
             int_bits = f.bits << (9 + e % (e > 63 ? 63 : 31) - 1);
-            dst->bits[e > 63 ? 1 : 0] = int_bits;
+            dst->bits[e / 32] = int_bits;
         } else { // пока для экспоненты в пределах одного инта
             s21_decimal int_part = {0, 0, 0, 0};
             s21_decimal frac_part = {0, 0, 0, 0};
@@ -64,47 +64,42 @@ int s21_from_float_to_decimal(float src, s21_decimal *dst) {   // перевод
 
 int s21_from_decimal_to_int(s21_decimal src, int *dst) { // нужен truncate
     int res = 0;
-    s21_decimal ten = {0b1010, 0, 0, 0};
-    s21_decimal max_int = {0xffffffff, 0, 0, 0};
-    int e = get_exp(src);
-    
-    res = s21_truncate(src, &src);
-    while (src.bits[1] || src.bits[2]) s21_sub(src, max_int, &src);
-
-    *dst = (int)src.bits[0] * (get_sign(src) ? -1 : 1);
+    s21_decimal int_part;
+    s21_truncate(src, &int_part);
+    if (int_part.bits[1] || int_part.bits[2]) {
+        *dst = 0;
+        res = 1;
+    } else {
+        *dst = int_part.bits[0];
+        *dst *= get_sign(int_part) ? -1 : 1;
+    }
 
     return res;
 }
 
 int s21_from_decimal_to_float(s21_decimal src, float *dst) {
+    int res = 0;
+    f_bits f;
+    f.bits = 0;
     s21_decimal int_part;
     s21_decimal frac_part;
-    s21_decimal ten = {0b1010, 0, 0, 0};
-    int e = get_exp(src);
-    int res = s21_truncate(src, &int_part);
-    res = s21_pow(ten, e, &ten);
-    res = s21_mul(src, ten, &src);
-    res = s21_sub(src, int_part, &frac_part);
-    unsigned j = 96;
-    unsigned pos = 0;
-    f_bits f = {0.0};
-    for (int i = 2; i >= 0 && !pos; --i) {
-        unsigned mask = 1 << 31;
-        while (mask && !pos) {
-            if (mask & int_part.bits[i]) pos = j;
-            mask >>= 1;
-            --j;
-        }
-    }
-    set_bit(&int_part, pos - 1, 0);
-    unsigned exp = pos - 1;
-    f.bits |= exp << 23;
-    int shift = 0;
-    while (pos) {
-        f.bits |= src.bits[(pos - 1) / 32] >> (9 + 32 * shift);
-        pos -= 32;
-        ++shift;
-    }
+    s21_truncate(src, &int_part);
+    s21_sub(src, int_part, &frac_part);
 
+    s21_decimal zero = {0, 0, 0, 0};
+    int pos = -1;
+    if (s21_is_not_equal(int_part, zero)) {
+        pos = 95 - mantiss_prev_nulls(int_part);
+        set_bit(&int_part, pos, 0);
+        f.bits = (pos + 127) << 24;
+        f.bits |= get_sign(int_part) ? (1 << 31) : 0;
+        if (pos % 32 >= 23 && pos % 32 <= 31) {
+            f.bits |= pos < 23 ? int_part.bits[pos / 32] << (23 - pos) : int_part.bits[pos / 32] >> (pos - 23);
+        } else {}
+        if (pos > 63) {
+            f.bits |= pos < 23 ? int_part.bits[0] << (23 - pos) : int_part.bits[0] >> (pos - 23);
+        }
+        f.bits |= pos < 23 ? int_part.bits[0] << (23 - pos) : int_part.bits[0] >> (pos - 23);
+    }
     return res;
 }
