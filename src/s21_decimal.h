de706fd85e28d1ea15709e5d570e8df72fcc957c
yuccierh <yuccierh@student.21-school.ru>
2024-02-25 14:53:47 +0300

    comment

--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -3,7 +3,7 @@
 
 #include <math.h>
 #include <stdbool.h>
-#include <stdio.h>  // <<<<<<<<<<<<<<<<<<<<<<<<<==========================<<<< УДАЛИТЬ ПЕРЕД СДАЧЕЙ
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
